beefa9e0dacde1baeafb6957bbe25f6c8c8fa5d2
chaadow <chaadow@msn.com>
2025-12-10 22:22:39 +0100

    Fix rails warnings

--- lib/view_component/configurable.rb
+++ lib/view_component/configurable.rb
@@ -4,6 +4,20 @@ module ViewComponent
   module Configurable
     extend ActiveSupport::Concern
 
+    class_methods do
+      def config
+        @_config ||= if respond_to?(:superclass) && superclass.respond_to?(:config)
+          superclass.config.inheritable_copy
+        else
+          ActiveSupport::OrderedOptions.new
+        end
+      end
+
+      def configure
+        yield config
+      end
+    end
+
     included do
       next if respond_to?(:config) && config.respond_to?(:view_component) && config.respond_to_missing?(:test_controller)
 
@@ -11,6 +25,10 @@ module ViewComponent
 
       configure do |config|
         config.view_component ||= ActiveSupport::InheritableOptions.new
+      end
+
+      def config
+        self.class.config
       end
     end
   end
